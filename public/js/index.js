6a1bb62076109d2390f1d25994a0d8b3f6af1811
Victoria Barr <vb329@georgetown.edu>
2014-11-22 22:18:04 +0000

    Changed a lot of visuals; replaced CSS hover action with JQuery; need to add buffer sections instead of padding to images.

---
+++ public/js/index.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+	$('#github-icon').mouseover(function(){
+		$('#github-icon').fadeTo('fast', 1);
+	});
+
+	$('#github-icon').mouseleave(function(){
+        $('#github-icon').fadeTo('fast', 0.4);    
+   });
+
+	$('#email-icon').mouseover(function(){
+		$('#email-icon').fadeTo('fast', 1);
+	});
+
+	$('#email-icon').mouseleave(function(){
+        $('#email-icon').fadeTo('fast', 0.4);    
+   });
+
+	$('#twitter-icon').mouseover(function(){
+		$('#twitter-icon').fadeTo('fast', 1);
+	});
+
+	$('#twitter-icon').mouseleave(function(){
+        $('#twitter-icon').fadeTo('fast', 0.4);    
+   });
+
+	$('#art-icon').mouseover(function(){
+		$('#art-icon').fadeTo('fast', 1);
+	});
+
+	$('#art-icon').mouseleave(function(){
+        $('#art-icon').fadeTo('fast', 0.4);    
+   });
+});
